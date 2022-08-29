050395be927fca23d003c06f248658b6ad8695e6
Hadi Rizaldi <hadirizaldi.hr@gmail.com>
2022-08-30 04:42:07 +0700

    proscess controller posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -9,28 +9,33 @@
         </div>
     </div>
 
-    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
-        <div>
-            <img src="https://cdn.pixabay.com/photo/2015/01/08/18/25/desk-593327_960_720.jpg" alt="">
+    @foreach ($posts as $post)
+
+        <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2015/01/08/18/25/desk-593327_960_720.jpg" alt="">
+            </div>
+            <div>
+                <h2 class="text-gray-700 font-bold text-5xl pb-4">
+                    {{ $post->title }}
+                </h2>
+
+                <span class="text-gray-500">
+                    By <span class="font-bold italic text-gray-800">Hadi Rizaldi
+                    </span>, 1 day ago
+                </span>
+
+                <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                    {{ $post->description}}
+                </p>
+
+                <a href="/blog/{{ $post-slug }}" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                    Keep Reading
+                </a>
+            </div>
         </div>
-        <div>
-            <h2 class="text-gray-700 font-bold text-5xl pb-4">
-                Learn how to write Laravel Code
-            </h2>
 
-            <span class="text-gray-500">
-                By <span class="font-bold italic text-gray-800">Hadi Rizaldi
-                </span>, 1 day ago
-            </span>
+    @endforeach
 
-            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim nam incidunt eaque sit, at ut adipisci alias illum maiores! A unde temporibus exercitationem asperiores alias quae ex veniam saepe perspiciatis?
-            </p>
-
-            <a href="" class="uppercase bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
-                Keep Reading
-            </a>
-        </div>
-    </div>
 
 @endsection
